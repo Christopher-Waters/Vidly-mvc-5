25e377e6db2c857f8110581b0b131b0db77c03e3
Chris Waters <chris.isaac.waters@gmail.com>
2019-09-25 21:30:17 -0400

    Section 4 Excericse

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -25,53 +25,41 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel()
+            var viewModel = new CustomerFormViewModel()
             {
                 Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);
+            return View("CustomerForm",viewModel);
         }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Save(Customer customer)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        var viewModel = new CustomerFormViewModel
-        //        {
-        //            Customer = customer,
-        //            MembershipTypes = _context.MembershipTypes.ToList()
-        //        };
 
-        //        return View("CustomerForm", viewModel);
-        //    }
-
-        //    if (customer.Id == 0)
-        //        _context.Customers.Add(customer);
-        //    else
-        //    {
-        //        var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
-        //        customerInDb.Name = customer.Name;
-        //        customerInDb.Birthdate = customer.Birthdate;
-        //        customerInDb.MembershipTypeId = customer.MembershipTypeId;
-        //        customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
-        //    }
-
-        //    _context.SaveChanges();
-
-        //    return RedirectToAction("Index", "Customers");
-        //}
-
+        
         public ViewResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id == 0)
+              _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
 
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Customers");
+        }
 
+      
         public ActionResult Details(int id)
         {
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
@@ -82,20 +70,22 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
-        //public ActionResult Edit(int id)
-        //{
-        //    var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-        //    if (customer == null)
-        //        return HttpNotFound();
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
-        //    var viewModel = new CustomerFormViewModel
-        //    {
-        //        Customer = customer,
-        //        MembershipTypes = _context.MembershipTypes.ToList()
-        //    };
+            if (customer == null)
+                return HttpNotFound();
 
-        //    return View("CustomerForm", viewModel);
-        //}
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+        
     }
 }
